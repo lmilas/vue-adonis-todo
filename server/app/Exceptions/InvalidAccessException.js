a1261abbfef60a3795141ac545a08c5b89035ca1
lmilas <lukasmilas@gmail.com>
2019-02-27 22:53:08 +0200

    destroy with exceptions

---
+++ server/app/Exceptions/InvalidAccessException.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const { LogicalException } = require('@adonisjs/generic-exceptions')
+
+class InvalidAccessException extends LogicalException {
+  /**
+   * Handle this exception by itself
+   */
+   handle (err, { response }) {
+     return response.status(403).json({
+       error: 'invalid access to resource',
+     });
+   }
+}
+
+module.exports = InvalidAccessException
